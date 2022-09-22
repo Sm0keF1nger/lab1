6721663e85ed2cc8f3654da87b9e3b1f7671b3ec
Sm0keF1nger <kkaptelov@gmail.com>
2022-09-22 13:25:29 +0300

    test

---
+++ ConsoleApplication2/ConsoleApplication2.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+
+	double x, y;
+	printf("insert x value: ");
+	scanf_s("%lf", &x);
+	y = pow(x, 3) - 4 * pow(x, 2) - 5 * x + 9 + cos(x);
+	printf("y = x^3 - 4x^2 - 5x + 9 + cos x\nresult: y = %.2lf\n", y);
+
+	return 0;
+}
